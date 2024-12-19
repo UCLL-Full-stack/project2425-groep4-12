43b3517e67a74705e5f5d118478bf24422189304
MengMassart_UCLL <113979804+Lycanthr0py@users.noreply.github.com>
2024-12-19 16:14:16 +0100

    Second batch

--- front-end/pages/training/index.tsx
+++ front-end/pages/training/index.tsx
@@ -4,6 +4,9 @@ import { useState, useEffect } from "react";
 import styles from "../../styles/home.module.css"
 import useInterval from "use-interval";
 
+import { useTranslation } from 'next-i18next';
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
 import { Training } from "@types";
 
 const TrainingPage: React.FC = () => {
@@ -16,5 +19,15 @@ const TrainingPage: React.FC = () => {
         </>
     )
 }
+
+export const getServerSideProps = async (context: { locale: any }) => {
+    const {locale} = context;
+  
+    return {
+        props: {
+            ...(await serverSideTranslations(locale ?? "en", ["common"])),
+        },
+    };
+};
 
 export default TrainingPage;
