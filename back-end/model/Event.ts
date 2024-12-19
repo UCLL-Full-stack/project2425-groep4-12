d0c8d29d42ff1c625e7d283a5578cecb6ec61023
Thomas Bosmans <thomas.bosmans@student.ucll.be>
2024-12-20 00:55:24 +0100

    extra checks for date in event

--- back-end/model/Event.ts
+++ back-end/model/Event.ts
@@ -54,6 +54,15 @@ export class Event {
         if (!event.start || !event.end) {
             throw new Error('Start and end date are required');
         }
+        if (event.start < new Date()) {
+            throw new Error('Start date cannot be in the past');
+        }
+        if (event.end < new Date()) {
+            throw new Error('End date cannot be in the past');
+        }
+        if (event.start === event.end) {
+            throw new Error('Start and end date cannot be the same');
+        }
         if (event.start > event.end) {
             throw new Error('Start date cannot be after end date');
         }
